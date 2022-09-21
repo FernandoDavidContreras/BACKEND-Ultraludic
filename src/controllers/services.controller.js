93f47fe453e321bfd70017a550570a2c49b83d14
fernando_moran <fernandomorann999@gmail.com>
2022-09-21 01:04:39 -0600

    agregamos una ruta para Agregar servicios

--- src/controllers/services.controller.js
+++ src/controllers/services.controller.js
@@ -28,6 +28,24 @@ const getService = async (req, res) => {
   }
 }
 
+const addService = async (req, res) => {
+  try {
+    const { name } = req.body
+    if (name === undefined) {
+      res.status(400).json({ message: 'Bad request. Please fill all field.' })
+    }
+
+    await Servicios.create({
+      name
+    })
+
+    res.json({ message: 'service add' })
+  } catch (error) {
+    res.status(500)
+    res.send(error.message)
+  }
+}
+
 // ruta para hcer la relacion de la tabla servicios con la tabla costos
 const getServiciosCosto = async (req, res) => {
   try {
@@ -57,5 +75,6 @@ const getServiciosCosto = async (req, res) => {
 export const methods = {
   getServices,
   getService,
+  addService,
   getServiciosCosto
 }
--- src/routes/services.routes.js
+++ src/routes/services.routes.js
@@ -8,6 +8,8 @@ routerServices.get('/', services.getServices)
 // ruta para acceder a un dato los datos de la tabla servicios de la base de datos.
 routerServices.get('/:id', services.getService)
 
+routerServices.post('/', services.addService)
+
 routerServices.get('/:id/costos', services.getServiciosCosto)
 
 export default routerServices
