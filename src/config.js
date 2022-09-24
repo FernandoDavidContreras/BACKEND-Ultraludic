1cdc25a01395abcf7da13bf581c20bcb565449d3
fernando_moran <fernandomorann999@gmail.com>
2022-09-24 00:23:33 -0600

    agregamos el puerto a la conexion sequelize

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ config()
 export const PORT = process.env.PORT || 4000
 
 export default {
-  port: process.env.PORT_DB || 3000,
+  port: process.env.PORT_DB || 3306,
   host: process.env.HOST || 'localhost',
   database: process.env.DATABASE || 'ultraludic_bd',
   user: process.env.USER || 'root',
--- src/database/database.js
+++ src/database/database.js
@@ -4,5 +4,6 @@ import config from './../config.js'
 // conectamos la base de datos
 export const sequelize = new Sequelize(config.database, config.user, config.password, {
   host: config.host,
-  dialect: 'mysql'
+  dialect: 'mysql',
+  port: config.port
 })
