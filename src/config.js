541b3f5b050b597648ff8afabbafb10569a5de13
fernando_moran <fernandomorann999@gmail.com>
2022-09-23 21:47:25 -0600

    añdiimos comando start

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ import { config } from 'dotenv'
 config()
 
 export default {
+  port: process.env.PORT || 3000,
   host: process.env.HOST || '',
   database: process.env.DATABASE || '',
   user: process.env.USER || '',
