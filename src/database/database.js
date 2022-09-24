407e4721fb65e44ef5242886d1b4755bfdf5dbeb
fernando_moran <fernandomorann999@gmail.com>
2022-09-24 00:48:25 -0600

    quitamos los mensjes por consola de la verificacion de la base de datos

--- src/database/database.js
+++ src/database/database.js
@@ -5,19 +5,5 @@ import config from './../config.js'
 export const sequelize = new Sequelize(config.database, config.user, config.password, {
   host: config.host,
   dialect: 'mysql',
-  port: config.port,
-  define: {
-    // charset: 'utf8mb4',
-    // collate: 'utf8mb4_unicode_ci'
-  },
-  dialectOptions: {
-    // charset: 'utf8mb4'
-  },
-  logging: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+  port: config.port
 })
--- src/index.js
+++ src/index.js
@@ -11,8 +11,6 @@ import './../models/costosHardaware.js'
 const main = async () => {
   try {
     await sequelize.sync()
-    await sequelize.authenticate()
-    console.log('connection to databse')
     app.listen(app.get('port'))
     console.log(`Server on port ${app.get('port')}`)
   } catch (error) {
