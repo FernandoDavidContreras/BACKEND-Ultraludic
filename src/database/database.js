655e5b379bfbfd1c39b5c6fbec352e04f4386a66
fernando_moran <fernandomorann999@gmail.com>
2022-09-24 00:38:55 -0600

    agregamos solucion de conexion

--- src/database/database.js
+++ src/database/database.js
@@ -5,5 +5,19 @@ import config from './../config.js'
 export const sequelize = new Sequelize(config.database, config.user, config.password, {
   host: config.host,
   dialect: 'mysql',
-  port: config.port
+  port: config.port,
+  define: {
+    // charset: 'utf8mb4',
+    // collate: 'utf8mb4_unicode_ci'
+  },
+  dialectOptions: {
+    // charset: 'utf8mb4'
+  },
+  logging: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 })
